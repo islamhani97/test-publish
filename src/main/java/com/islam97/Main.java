3b6fcc44297ff1792516989e73bddf6b54cef04f
Islam Hani <72826869+islamhani97@users.noreply.github.com>
2023-07-25 13:36:40 +0300

    Add files

---
+++ src/main/java/com/islam97/Main.java
@@ -0,0 +1,12 @@
+package com.islam97;
+
+import com.islam97.textgenerator.TextGenerator;
+
+public class Main {
+    public static void main(String[] args) {
+
+        TextGenerator textGenerator = new TextGenerator();
+        System.out.println(textGenerator.generateText());
+
+    }
+}
---
+++ textgenerator/src/main/java/com/islam97/textgenerator/TextGenerator.java
@@ -0,0 +1,8 @@
+package com.islam97.textgenerator;
+
+public class TextGenerator {
+
+    public String generateText() {
+        return "The text has been generated from TextGenerator...";
+    }
+}
